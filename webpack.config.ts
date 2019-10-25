bcdbb216a04fafc33ca877beae8aace5b574f745
Baldmort <lord.baldmort@gmail.com>
2019-10-26 00:54:32 +0900

    [config] Set ESLint

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const webpackConfig: webpack.Configuration = {
+  mode: 'production',
+  entry: {
+    architekton: './architekton.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
+
+export default webpackConfig;
